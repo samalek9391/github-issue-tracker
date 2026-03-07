3dfa1171291d6f9f8380be7ec93187c3899aca31
samalek9391 <sa.malek9391@gmail.com>
2026-03-07 20:33:48 +0600

    card is loaded dynamically in test

--- script.js
+++ script.js
@@ -14,3 +14,39 @@ signBtn.addEventListener("click", function(){
     }
 })
 
+// const toggleSection = document.getElementById("toogle-section");
+
+// async function loadButtons () {
+//     const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+//     const data = await res.json();
+//     console.log(data.data);
+//     console.log(toggleSection);
+//     data.data.forEach(element => {
+//         console.log(element);
+//         const btn = document.createElement("button");
+//         btn.className = "btn btn-outline px-10 py-3";
+//         toggleSection.appendChild(btn);
+//     });
+// }
+
+// loadButtons()
+
+const issuesCards = document.getElementById("issues-cards");
+// console.log(issuesCards);
+
+async function loadCards() {
+    const res = await fetch ("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    const data = await res.json();
+    // console.log(data);
+    data.data.forEach(card => {
+        // console.log(card);
+        const issueCard = document.createElement("div");
+        // console.log(issueCard);
+        issueCard.innerHTML = `
+        <h2>This is card${card.title}</h2>
+        `;
+        issuesCards.append(issueCard);
+    });
+}
+
+loadCards()
