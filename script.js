a3d211dc262950ebf06577055e172fc3f8caea31
samalek9391 <sa.malek9391@gmail.com>
2026-03-09 19:35:04 +0600

    complete all funtionality

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const signBtn = document.getElementById("signin-btn");
 const loader = document.getElementById("loader");
+const issuesCards = document.getElementById("issues-cards");
+const allCount = document.getElementById("all-count");
+const tabs = document.querySelectorAll(".tab-btn");
+const searchInput = document.getElementById("search-input");
 
 // Sign In authintication
-
 signBtn.addEventListener("click", function () {
   const userName = document.getElementById("username").value;
   const password = document.getElementById("password").value;
@@ -19,21 +22,7 @@ signBtn.addEventListener("click", function () {
 });
 
 
-const issuesCards = document.getElementById("issues-cards");
-const allCount = document.getElementById("all-count");
-
-// async function loadCards() {
-//   const res = await fetch(
-//     "https://phi-lab-server.vercel.app/api/v1/lab/issues",
-//   );
-//   const data = await res.json();
-
-//   allCount.innerText = data.data.length;
-//   displayCards(data.data);
-// }
-
-
-
+// load cards
 async function loadCards() {
   loader.classList.remove("hidden")
   const res = await fetch(
@@ -51,14 +40,11 @@ async function loadCards() {
 }
 
 
-
 // Display all cards to dashboard
-
 function displayCards(cards) {
   issuesCards.innerHTML = "";
 
   for (const card of cards) {
-
     const issueCard = document.createElement("div");
 
     const borderColor =
@@ -67,10 +53,9 @@ function displayCards(cards) {
     const statusIcon =
       card.status === "open"
         ? "./assets/Open-Status.png"
-        : "./assets/Closed- Status .png";
+        : "./assets/Closed-Status.png";
 
     issueCard.innerHTML = `
-    
     <div class="bg-[#F8FAFC] p-4 rounded-lg shadow border-t-4 ${borderColor} cursor-pointer">
 
         <div class="flex justify-between items-center mb-3">
@@ -89,15 +74,19 @@ function displayCards(cards) {
         ${card.description}
         </p>
 
-        <div class="flex gap-2 mb-3 border-b p-5">
+        <div class="flex gap-2 mb-3 border-b pb-3">
             ${card.labels
-              .map(
-                (label) => `
-            <span class="text-xs bg-gray-200 px-2 py-1 rounded">
-            ${label}
-            </span>
-            `
-              )
+              .map((label) => {
+                const labelColor =
+                  label === "bug" ? "bg-[#FEECEC]" : label === "help wanted" ? "bg-[#FFF8DB]" : "bg-[#DEFCE8]";
+                const textColor = 
+                  label === "bug" ? "text-[#EF4444]" : label === "help wanted" ? "text-[#D97706]" : "bg-[#00A96E]";
+                return `
+                <span class="text-xs ${labelColor} ${textColor} px-2 py-1 rounded">
+                  ${label}
+                </span>
+                `;
+              })
               .join("")}
         </div>
 
@@ -105,23 +94,20 @@ function displayCards(cards) {
         <p class="text-xs text-gray-400">${card.createdAt}</p>
 
     </div>
-    
     `;
-    
+
+    issueCard.addEventListener("click", () => {
+      openModal(card.id);
+    });
+
     issuesCards.appendChild(issueCard);
   }
-};
+}
 
 
 let allIssues = [];
 
-
-
-
 // button toggling
-
-const tabs = document.querySelectorAll(".tab-btn");
-
 for (const tab of tabs) {
   tab.addEventListener("click", function () {
     
@@ -145,17 +131,75 @@ for (const tab of tabs) {
 
 loadCards();
 
+// Modal
+async function openModal(id) {
+
+  const res = await fetch(
+    `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`
+  );
+
+  const data = await res.json();
+
+  const issue = data.data;
+
+ 
+  document.getElementById("modal-title").innerText = issue.title;
+  const status = document.getElementById("modal-status");
+  status.innerText = issue.status;
+
+  if(issue.status === "open"){
+    status.className = "px-3 py-1 rounded-full text-white bg-green-500";
+  }else{
+    status.className = "px-3 py-1 rounded-full text-white bg-purple-500";
+  }
+
+  document.getElementById("modal-author").innerText =
+    "Opened by " + issue.author;
+  document.getElementById("modal-date").innerText =
+    new Date(issue.createdAt).toLocaleDateString();
+
+
+  document.getElementById("modal-description").innerText =
+    issue.description;
+
+  document.getElementById("modal-assignee").innerText =
+    issue.author;
+
+  const priority = document.getElementById("modal-priority");
+  priority.innerText = issue.priority;
+
+  if(issue.priority === "high"){
+    priority.className = "px-3 py-1 rounded-full text-white bg-red-500";
+  }else{
+    priority.className = "px-3 py-1 rounded-full text-white bg-yellow-500";
+  }
+
+  const labels = document.getElementById("modal-labels");
+
+  labels.innerHTML = "";
+
+  for(const label of issue.labels){
+    const span = document.createElement("span");
+    span.className = label === "bug" ? "bg-red-200 px-2 py-1 text-xs rounded" : label === "help wanted" ? "bg-yellow-200 px-2 py-1 text-xs rounded" : "bg-green-200 px-2 py-1 text-xs rounded";
+    span.innerText = label;
+    labels.appendChild(span);
+  }
+
+  document.getElementById("issueModal").showModal();
+}
+
 
 // Search Functionality
+searchInput.addEventListener("keyup", () => {
 
-const searchInput = document.getElementById("search-input");
+  const text = searchInput.value.toLowerCase();
 
-searchInput.addEventListener("keyup", async () => {
-  const text = searchInput.value;
-  const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${text}`);
-  const data = await res.json();
-  displayCards(data.data);
+  const filtered = allIssues.filter(issue =>
+    issue.title.toLowerCase().includes(text) ||
+    issue.description.toLowerCase().includes(text)
+  );
 
+  displayCards(filtered);
 });
 
 
